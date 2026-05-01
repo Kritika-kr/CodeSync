b2830a6e15ca6701ed2805f250193a81bc6cdc4a
Kritika Kumari <kritika3073@gmail.com>
2026-05-01 12:07:17 +0530

    k

--- collab-code/client/src/api/runCode.js
+++ collab-code/client/src/api/runCode.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://collab-code-platform.onrender.com";
 
 export const runCode = async (code, language) => {
   try {
